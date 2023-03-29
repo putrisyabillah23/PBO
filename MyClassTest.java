d9525c4b471b4883ab80c9f7e811251587b5a8bd
putrisyabillah23 <113838961+putrisyabillah23@users.noreply.github.com>
2023-03-29 21:11:10 +0700

    Create MyClassTest.java

---
+++ MyClassTest.java
@@ -0,0 +1,14 @@
+public class MyClassTest {
+
+	public static void main (String[] args) {
+		MyClass myClass = new MyClass();
+		myClass.setId("0642215");
+		myClass.setName("Putri Syabillah");
+		myClass.setAge(19);
+		
+		System.out.println("ID 	: " + myClass.getId());
+		System.out.println("Name	: " + myClass.getName());
+		System.out.println("Age 	: " + myClass.getAge());
+		
+	}
+}
